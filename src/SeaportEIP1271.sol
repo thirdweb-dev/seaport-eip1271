e81fe4ba8c9ef6cd47979ec45372d932b1d91f62
Krishang <krishang@thirdweb.com>
2024-03-12 14:51:40 +0530

    Add replay protection for validation of regular ECDSA signatures

--- src/SeaportEIP1271.sol
+++ src/SeaportEIP1271.sol
@@ -8,6 +8,12 @@ import {OrderParameters} from "seaport-types/src/lib/ConsiderationStructs.sol";
 abstract contract SeaportEIP1271 is SeaportOrderParser {
     using ECDSA for bytes32;
 
+    bytes32 private constant ACCOUNT_MESSAGE_TYPEHASH = keccak256("AccountMessage(bytes message)");
+    bytes32 private constant EIP712_TYPEHASH =
+        keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)");
+    bytes32 private immutable HASHED_NAME = keccak256("Account");
+    bytes32 private immutable HASHED_VERSION = keccak256("1");
+
     /// @notice The function selector of EIP1271.isValidSignature to be returned on sucessful signature verification.
     bytes4 public constant MAGICVALUE = 0x1626ba7e;
 
@@ -45,7 +51,7 @@ abstract contract SeaportEIP1271 is SeaportOrderParser {
                 targetSig[i] = extractedPackedSig[i];
             }
         } else {
-            targetDigest = _message;
+            targetDigest = getMessageHash(_message);
             targetSig = _signature;
         }
 
@@ -54,6 +60,21 @@ abstract contract SeaportEIP1271 is SeaportOrderParser {
         if (_isAuthorizedSigner(signer)) {
             magicValue = MAGICVALUE;
         }
+    }
+
+    /**
+     * @notice Returns the hash of message that should be signed for EIP1271 verification.
+     * @param _hash The message hash pre replay protection.
+     * @return messageHash The digest (with replay protection) to sign for EIP-1271 verification.
+     */
+    function getMessageHash(bytes32 _hash) public view returns (bytes32) {
+        bytes32 messageHash = keccak256(abi.encode(_hash));
+        bytes32 typedDataHash = keccak256(abi.encode(ACCOUNT_MESSAGE_TYPEHASH, messageHash));
+        return keccak256(abi.encodePacked("\x19\x01", _accountDomainSeparator(), typedDataHash));
+    }
+
+    function _accountDomainSeparator() private view returns (bytes32) {
+        return keccak256(abi.encode(EIP712_TYPEHASH, HASHED_NAME, HASHED_VERSION, block.chainid, address(this)));
     }
 
     /// @notice Returns whether a given signer is an authorized signer for the contract.
